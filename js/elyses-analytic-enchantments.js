790e8a606ab5c2b1f1dee02de0492468a69592f1
Tatiana Kh <ts.khol@gmail.com>
2022-06-20 16:15:34 +0300

    js: solve elyses analytic enchantments

---
+++ js/elyses-analytic-enchantments.js
@@ -0,0 +1,19 @@
+export const getCardPosition = (stack, card) => stack.indexOf(card)
+
+export const doesStackIncludeCard = (stack, card) => stack.includes(card)
+
+export function isEachCardEven(stack) {
+  return stack.every((num) => num % 2 === 0)
+}
+
+export function doesStackIncludeOddCard(stack) {
+  return stack.some((num) => num % 2 !== 0)
+}
+
+export function getFirstOddCard(stack) {
+  return stack.find((num) => num % 2 !== 0)
+}
+
+export function getFirstEvenCardPosition(stack) {
+  return stack.findIndex((num) => num % 2 === 0)
+}

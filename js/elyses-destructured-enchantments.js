a7e6c219641cf319c6042ecd9e130e0412d32143
Tatiana Kh <ts.khol@gmail.com>
2022-06-28 21:38:57 +0300

    js: solve elyses destructured enchantments

---
+++ js/elyses-destructured-enchantments.js
@@ -0,0 +1,27 @@
+export function getFirstCard(deck) {
+  return deck[0]
+}
+
+export function getSecondCard(deck) {
+  const  [ , secondCard] = deck
+  return secondCard
+}
+
+export function swapTopTwoCards(deck) {
+  [deck[0], deck[1]] = [deck[1], deck[0]]
+  return deck
+}
+
+export function discardTopCard(deck) {
+  const [firstCard, ...rest] = deck
+  const otherCards = [...rest]
+
+  return [firstCard, otherCards]
+}
+
+export function insertFaceCards(deck) {
+  const faceCards = ['jack', 'queen', 'king']
+  const [firstCard, ...otherСards] = deck
+ 
+  return [firstCard, ...faceCards, ...otherСards]
+}

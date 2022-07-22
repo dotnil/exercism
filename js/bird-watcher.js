5be4a651951a3aa23d1d03d787bcdd528a3a4f6f
Tatiana Kh <ts.khol@gmail.com>
2022-07-22 21:42:34 +0300

    js: solve bird watcher

---
+++ js/bird-watcher.js
@@ -0,0 +1,24 @@
+export function totalBirdCount(birdsPerDay) {
+  let sum = birdsPerDay.reduce(function (a, b) {
+    return a + b
+  })
+  return sum
+}
+
+export function birdsInWeek(birdsPerDay, week) {
+  const startIndex = (week - 1) * 7
+  const weekArray = birdsPerDay.slice(startIndex, startIndex + 7)
+
+  return weekArray.reduce((sum, day) => day + sum)
+}
+
+export function fixBirdCountLog(birdsPerDay) {
+  birdsPerDay.forEach(function (day, index) {
+    const isOdd = index % 2 === 0
+    if (isOdd) {
+      birdsPerDay[index] = day + 1
+    }
+  })
+
+  return birdsPerDay
+}

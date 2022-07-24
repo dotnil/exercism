ef60b32ddc3c304e695561fa5f08b159e9c00975
Tatiana Kh <ts.khol@gmail.com>
2022-07-24 22:35:24 +0300

    js: solve Annalyn's infiltratioi

---
+++ js/annalyns-infiltrations.js
@@ -0,0 +1,22 @@
+export function canExecuteFastAttack(knightIsAwake) {
+  return !knightIsAwake
+}
+
+export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
+  return knightIsAwake || prisonerIsAwake || archerIsAwake
+}
+
+export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
+  return !archerIsAwake && prisonerIsAwake
+}
+
+export function canFreePrisoner(
+  knightIsAwake,
+  archerIsAwake,
+  prisonerIsAwake,
+  petDogIsPresent
+) {
+  const withDog = !archerIsAwake && petDogIsPresent
+  const withoutDog = !petDogIsPresent && prisonerIsAwake && !knightIsAwake && !archerIsAwake
+  return withDog || withoutDog
+}

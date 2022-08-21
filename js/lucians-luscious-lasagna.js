ea484f2c28ce2a890358dc4b1f2275189b696975
Tatiana Kh <ts.khol@gmail.com>
2022-08-21 23:59:25 +0300

    js: solve Lucian's Luscious Lasagna

---
+++ js/lucians-luscious-lasagna.js
@@ -0,0 +1,16 @@
+// https://exercism.org/tracks/javascript/exercises/lasagna/solutions/dotnil
+
+const PREPARATION_MINUTES_PER_LAYER = 2
+export const EXPECTED_MINUTES_IN_OVEN = 40
+
+export function remainingMinutesInOven(actualMinutesInOven) {
+  return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven
+}
+
+export function preparationTimeInMinutes(numberOfLayers) {
+  return numberOfLayers * PREPARATION_MINUTES_PER_LAYER
+}
+
+export function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
+  return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven
+}

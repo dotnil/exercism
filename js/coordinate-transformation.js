667e89697511d5046c139f55b6746f626eae5fae
Tatiana Kh <ts.khol@gmail.com>
2022-08-26 14:05:31 +0300

    js: solve of Coordinate Transformation

--- js/coordinate-transformation.js
+++ js/coordinate-transformation.js
@@ -1,3 +1,5 @@
+// https://exercism.org/tracks/javascript/exercises/coordinate-transformation/solutions/dotnil
+
 export function translate2d(dx, dy) {
   return (x, y) => [x + dx, y + dy]
 }
@@ -10,5 +12,20 @@ export function composeTransform(f, g) {
   return (x, y) => {
     const fResult = f(x, y)
     return g(...fResult)
+  }
+}
+
+export function memoizeTransform(f) {
+  let cache
+  let cached_y
+  let cached_x
+
+  return (x,y) => {
+    if (x == cached_x && y == cached_y) { return cache }
+
+    cached_y = y
+    cached_x = x
+    cache = f(x,y)
+    return cache
   }
 }

b47375946b4125328fb62957e4309e7109f316dd
Tatiana Kh <ts.khol@gmail.com>
2022-11-16 23:40:24 +0300

    js: solve of Perfect Numbers

---
+++ js/perfect-numbers.js
@@ -0,0 +1,15 @@
+export const classify = n => {
+
+  function aliquotSum(n) {
+    const sum = 
+   
+  }
+
+  if (n < 1) {
+    throw new Error('Classification is only possible for natural numbers.')
+  }
+
+  if (aliquotSum = n) { return 'perfect' }
+  if (aliquotSum > n) { return 'abundant' }
+  { return 'deficient' }
+}

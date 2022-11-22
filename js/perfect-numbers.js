3673c372539d43a17356bf70d70eb704eb3bafa8
Tatiana Kh <ts.khol@gmail.com>
2022-11-22 18:19:10 +0300

    js: update solve of 'Perfect Numbers'

--- js/perfect-numbers.js
+++ js/perfect-numbers.js
@@ -1,15 +1,18 @@
-export const classify = n => {
+// https://exercism.org/tracks/javascript/exercises/perfect-numbers/solutions/dotnil
 
-  function aliquotSum(n) {
-    const sum = 
-   
-  }
+const aliquotSum = num => {
+  let sum = 0
+  for (let i = 1; i < num; i++)
+    if (num % i == 0)
+      sum += i
 
-  if (n < 1) {
-    throw new Error('Classification is only possible for natural numbers.')
-  }
+  return sum
+}
 
-  if (aliquotSum = n) { return 'perfect' }
-  if (aliquotSum > n) { return 'abundant' }
-  { return 'deficient' }
+export const classify = num => {
+  if (num < 1) throw new Error('Classification is only possible for natural numbers.')
+
+  if (aliquotSum(num) === num) return 'perfect'
+  if (aliquotSum(num) > num) return 'abundant'
+  return 'deficient'
 }

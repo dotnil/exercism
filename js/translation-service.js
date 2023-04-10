0c544abdeb870229bb1ba94fa6179bb251d072d4
Tatiana Kh <ts.khol@gmail.com>
2023-04-10 20:32:36 +0300

    js: update solve of Translation service

--- js/translation-service.js
+++ js/translation-service.js
@@ -3,27 +3,102 @@ export class TranslationService {
     this.api = api
   }
 
-  async free(text) {
-    try {
-      const response = await this.api.fetch(text)
-      return response.translation
-    }
-    catch(error) {
-      return Promise.reject(error)
-    }
+  free(text) {
+    return this.api.fetch(text).then(obj => obj.translation)
   }
 
-  async batch(texts) {
-    try {
-      if (texts.length === 0) { throw new BatchIsEmpty }
+  batch(texts) {
+    return new Promise((resolve) => {
+      if (texts.length === 0) { throw new BatchIsEmpty() }
+
       const promises = texts.map(text => this.free(text))
-      return await Promise.all(promises)
-    }
-    catch(error) {
-      return Promise.reject(error)
-    }
+      resolve(Promise.all(promises).then(values => values))
+    })
   }
 
 
+  request(text) {
+    return this.retryRequest(text, 2)
+  }
 
+  retryRequest(text, tries) {
+    return new Promise((resolve, reject) => {
+      this.api.request(text, result => {
+        if (result === undefined) { return resolve(undefined) }
+        if (tries == 0) { return reject(result) }
+
+        resolve(this.retryRequest(text, tries - 1))
+      })
+    })
+  }
+
+
+  /**
+   * Retrieves the translation for the given text
+   *
+   * - Rejects with an error if the quality can not be met
+   * - Requests a translation if the translation is not available, then retries
+   *
+   * @param {string} text
+   * @param {number} minimumQuality
+   * @returns {Promise<string>}
+   */
+  premium(text, minimumQuality) {
+    return this
+      .fetchWithMinimumQuality(text, minimumQuality)
+      .catch(error => {
+        console.log(error)
+        if (error instanceof QualityThresholdNotMet) {
+          throw error
+        } else {
+          return this.request(text).then(() => this.fetchWithMinimumQuality(text, minimumQuality))
+        }
+      })
+  }
+
+  fetchWithMinimumQuality(text, minimumQuality) {
+    return this.api.fetch(text).then(obj => {
+      console.log(obj)
+      if (obj.quality >= minimumQuality) {
+        return obj.translation
+      }
+
+      throw new QualityThresholdNotMet()
+    })
+  }
+
+
+}
+
+/**
+ * This error is used to indicate a translation was found, but its quality does
+ * not meet a certain threshold. Do not change the name of this error.
+ */
+export class QualityThresholdNotMet extends Error {
+  /**
+   * @param {string} text
+   */
+  constructor(text) {
+    super(
+      `
+The translation of ${text} does not meet the requested quality threshold.
+    `.trim()
+    )
+
+    this.text = text
+  }
+}
+
+/**
+ * This error is used to indicate the batch service was called without any
+ * texts to translate (it was empty). Do not change the name of this error.
+ */
+export class BatchIsEmpty extends Error {
+  constructor() {
+    super(
+      `
+Requested a batch translation, but there are no texts in the batch.
+    `.trim()
+    )
+  }
 }
